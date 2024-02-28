96160b0d0ac50cb1bad6f8b60aab35de16b0134e
SandhyaMadhunagula <161038831+SandhyaMadhunagula@users.noreply.github.com>
2024-02-28 21:50:08 +0530

    Add files via upload

---
+++ java.com/arthmtc.java
@@ -0,0 +1,10 @@
+public static Main
+  {
+    public static void main(string[] args){
+     int a=10,b=2;
+       system.out.println("sum of two numbers is:"+(a+b));
+       system.out.println("difference of two numbers is:"+(a-b));
+       system.out.println("prduct of two numbers is:"+(a*b));
+       system.out.println("div.of two numbers is:"+(a/b));
+       }
+}
