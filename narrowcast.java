a32b85e039e76dc37a334b7715f630dacd4710bf
SandhyaMadhunagula <161038831+SandhyaMadhunagula@users.noreply.github.com>
2024-03-21 12:50:04 +0530

    Add files via upload

---
+++ narrowcast.java
@@ -0,0 +1,11 @@
+class narrowcast{
+public static void main(String[] args){
+int marks=119,sub=30;
+double dmarks=(double)marks;
+short subjct=(short)sub;
+System.out.print(marks);
+System.out.println(dmarks);
+System.out.println(subjct);
+}
+}
+
