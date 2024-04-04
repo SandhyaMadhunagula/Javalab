3b9b94a81cfab14021ffe7b60415f422e2e1abd0
Madhunagula Sandhya <161038831+SandhyaMadhunagula@users.noreply.github.com>
2024-04-04 12:18:03 +0530

    Add files via upload

---
+++ itrarray.java
@@ -0,0 +1,16 @@
+class itrarray{
+public static void main(String[] args){
+int[] arr={1,2,3,4,5,6,7};
+cmparray co=new cmparray();
+for (int l:co.arr3)
+{
+System.out.println(l);
+}
+cmparray.main(null);
+System.out.println("elemnets are:");
+for(int i=0;i<arr.length;i++)
+{
+System.out.println(arr[i]);
+}
+}
+}
