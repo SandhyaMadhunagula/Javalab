f52368d568e140e8225f0485dc366966c5533f5e
Madhunagula Sandhya <161038831+SandhyaMadhunagula@users.noreply.github.com>
2024-04-18 12:19:50 +0530

    Add files via upload
    == operator refers to the whether two strings are pointing to same memory location or not.
    if both objectsa refers to the same memory location then it is "true"
    or  else "false".

---
+++ strCon.java
@@ -0,0 +1,10 @@
+class strCon{
+  public static void main(String[] args){
+  String s1="Rohit";
+  String s2="Rohit";
+  String s3=new String("Rohit");
+  System.out.println(s1==s2);
+  System.out.println(s1==s3);
+
+  }
+}
