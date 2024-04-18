041e469f2df80ff6e0db17ec45a560b4a2ddfda3
Madhunagula Sandhya <161038831+SandhyaMadhunagula@users.noreply.github.com>
2024-04-18 11:43:41 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,8 @@
+class string{
+public static void main(String [] args){
+String str1="HEllo";
+String str2=new String(str1);
+System.out.println("string1 is "+str1);
+System.out.println("string2 is "+str2);
+}
+}
